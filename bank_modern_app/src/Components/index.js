180125a075d3658bd3a2ba2e44d692be7bfe7aed
Gabriel Souza <126912751+Gabr1elSouza@users.noreply.github.com>
2024-03-06 02:38:02 -0300

    init Project with navbar

---
+++ bank_modern_app/src/Components/index.js
@@ -0,0 +1,23 @@
+import Billing from "./Billing";
+import Business from "./Business";
+import CTA from "./CTA";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Stats from "./Stats";
+import Testiomonials from "./Testiomonials";
+
+export {
+  Billing,
+  Business,
+  CTA,
+  CardDeal,
+  Clients,
+  Footer,
+  Hero,
+  Navbar,
+  Stats,
+  Testiomonials,
+};
